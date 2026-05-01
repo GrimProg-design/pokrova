3b129807ee41d9092af58692563fd7ebb1980dad
GrimProg <vavilovila01@gmail.com>
2026-05-01 23:02:01 +0600

    Закончил расписание добавив все дни недели
    Добавил стили для расписания + адаптив

--- front/src/pages/Schedule/Schedule.tsx
+++ front/src/pages/Schedule/Schedule.tsx
@@ -1,5 +1,6 @@
 import type { JSX } from "react";
 import { useState, useEffect } from "react";
+import "./Schedule.css";
 
 interface ScheduleData {
   monday: string;
@@ -22,12 +23,49 @@ export default function Schedule(): JSX.Element {
   }, []);
 
   return (
-    <div>
-      {schedule.map((item, index) => (
-        <div key={index}>
-          <p>{item.monday}</p>
-        </div>
-      ))}
+    <div className="schedule-container">
+      <h2 className="schedule-title">Расписание на неделю</h2>
+
+      <div className="schedule-vertical">
+        {schedule.map((item, index) => (
+          <div className="schedule-card" key={index}>
+            <div className="schedule-row">
+              <span className="day">Понедельник</span>
+              <span className="value">{item.monday}</span>
+            </div>
+
+            <div className="schedule-row">
+              <span className="day">Вторник</span>
+              <span className="value">{item.tuesday}</span>
+            </div>
+
+            <div className="schedule-row">
+              <span className="day">Среда</span>
+              <span className="value">{item.wednesday}</span>
+            </div>
+
+            <div className="schedule-row">
+              <span className="day">Четверг</span>
+              <span className="value">{item.thursday}</span>
+            </div>
+
+            <div className="schedule-row">
+              <span className="day">Пятница</span>
+              <span className="value">{item.friday}</span>
+            </div>
+
+            <div className="schedule-row">
+              <span className="day">Суббота</span>
+              <span className="value">{item.saturday}</span>
+            </div>
+
+            <div className="schedule-row">
+              <span className="day">Воскресенье</span>
+              <span className="value">{item.sunday}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
